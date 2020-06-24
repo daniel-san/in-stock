7a4eb50a129d34fd39b63c852e795ec2149abced
Daniel Lucas <dlsbdaniel@gmail.com>
2020-06-24 10:46:01 -0300

    Add tests and mocks for stock availability

--- app/Models/Retailer.php
+++ app/Models/Retailer.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Clients\ClientFactory;
+use Facades\App\Clients\ClientFactory;
 
 class Retailer extends Model
 {
@@ -20,6 +20,6 @@ class Retailer extends Model
 
     public function client()
     {
-        return (new ClientFactory)->make($this);
+        return ClientFactory::make($this);
     }
 }
--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -2,14 +2,21 @@
 
 namespace Tests\Unit;
 
+use App\Clients\Client;
+use Facades\App\Clients\ClientFactory;
 use App\Clients\ClientNotFoundException;
+use App\Clients\StockStatus;
 use App\Models\Retailer;
 use App\Models\Stock;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use RetailerWithProductSeeder;
 use Tests\TestCase;
 
 class StockTest extends TestCase
 {
+
+    use RefreshDatabase;
+
     /** @test */
     public function it_throws_an_exception_if_a_client_is_not_found_when_tracking()
     {
@@ -20,5 +27,20 @@ class StockTest extends TestCase
         $this->expectException(ClientNotFoundException::class);
 
         Stock::first()->track();
+    }
+
+    /** @test */
+    public function it_updates_local_stock_status_after_being_tracked()
+    {
+        $this->seed(RetailerWithProductSeeder::class);
+
+        ClientFactory::shouldReceive('make->checkAvailability')->andReturn(
+            new StockStatus($available = true, $price = 9900)
+        );
+
+        $stock = tap(Stock::first())->track();
+
+        $this->assertTrue($stock->in_stock);
+        $this->assertEquals(9900, $stock->price);
     }
 }
